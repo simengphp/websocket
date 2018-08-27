e7cb5ffa4156366dc2068ee13ef1bb70ac32d98c
liuzhu <liuzhu@zanpu.com>
2018-08-27 10:47:13 +0800

    websocket

--- websocket.php
+++ websocket.php
@@ -7,7 +7,7 @@
      */
     /**WebSocket*/
     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-    socket_bind($socket, '127.0.0.1');
-    socket_connect($socket, '127.0.0.1', 8000);
+    socket_bind($socket, '0.0.0.0');
+    socket_connect($socket, '0.0.0.0', 8000);
     $ret = socket_listen($socket, 2);
-    dump($ret);
+    var_dump($ret);
